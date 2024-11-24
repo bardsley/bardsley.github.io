a8a69abfbb1d776f4509238dcfb9740bfebdefa0
Adam Bardsley <adam.bardsley@gmail.com>
2024-11-24 23:54:35 +0000

    Added measures and smoothed it all out v0.8

--- docs/client/winchester/attempt2.js
+++ docs/client/winchester/attempt2.js
@@ -54,7 +54,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
       trigger: '#introduction',
       start: 'top 66%',
       end: 'bottom bottom',
-      scrub: true,
+      scrub: 0.5,
       // markers: true,
     }
   })
@@ -85,7 +85,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const labelOutState = {x: "-100%", scale: "0", duration: duration}
   const labelOutTiming = "" //'-=0.2'
   const labelInState = {x: 0, scale: "1", duration: duration}
-  const labelInTiming = "+=0.2"
+  const labelInTiming = "" //+=0.2"
   
   tlGoals.addLabel("start")
     .add(gsap.to('#sports',{ y:50, duration: 1}),"start")
@@ -93,20 +93,59 @@ document.addEventListener("DOMContentLoaded", function(event) {
     .addLabel("community")
     .to("#community",{x: 0, scale: "1", duration: duration})
     .addLabel('support')
-    .to('#goal-wheel', { rotation: 90 })
+    .to('#goal-wheel', { rotation: 90, duration: duration },`support${labelOutTiming}`)
     .add(gsap.to("#community",labelOutState),`support${labelOutTiming}`)
     .add(gsap.to("#support",labelInState),`support${labelInTiming}`)
     .addLabel('advocacy')
-    .to('#goal-wheel', { rotation: 180 })
+    .to('#goal-wheel', { rotation: 180, duration: duration },`advocacy${labelOutTiming}`)
     .add(gsap.to("#support",labelOutState),`advocacy${labelOutTiming}`)
     .add(gsap.to("#advocacy",labelInState),`advocacy${labelInTiming}`)
     .addLabel('familiarity')
-    .to('#goal-wheel', { rotation: 270 })
+    .to('#goal-wheel', { rotation: 270, duration: duration },`familiarity${labelOutTiming}`)
     .add(gsap.to("#advocacy",labelOutState),`familiarity${labelOutTiming}`)
     .add(gsap.to("#familiarity",labelInState),`familiarity${labelInTiming}`)
     .addLabel('end')
-    .to('#goal-wheel', { rotation: 360 })
+    .to('#goal-wheel', { rotation: 360, duration: duration })
     .add(gsap.to("#familiarity",labelOutState),`end${labelOutTiming}`)
+
+  // Measures Wheel
+  const tlMeasures = gsap.timeline({
+    scrollTrigger: {
+      trigger: '#measures',
+      pin: true, 
+      start: 'top top', // when the top of the trigger hits the top of the viewport
+      end: '+=1500px', // end after scrolling 500px beyond the start
+      scrub: 0.5, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+      snap: {
+          snapTo: 'labels', // snap to the closest label in the timeline
+          // duration: { max: 0.3 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+          // delay: 0.1, // wait 0.2 seconds from the last scroll event before doing the snapping
+          ease: 'power4.inOut' // the ease of the snap animation ("power3" by default)
+      },
+      markers: true,
+    }
+  });
+  
+  tlMeasures.addLabel("start2")
+    .add(gsap.to('#students',{ y:50, duration: 1}),"start2")
+    // .add(gsap.to('#goals',{ paddingTop: "0px"}),"start")
+    .addLabel("belonging")
+    .to("#belonging",{x: 0, scale: "1", duration: duration})
+    .addLabel('engagement')
+    .to('#goal-wheel-2', { rotation: 90, duration: duration },`engagement${labelOutTiming}`)
+    .add(gsap.to("#belonging",labelOutState),`engagement${labelOutTiming}`)
+    .add(gsap.to("#engagement",labelInState),`engagement${labelInTiming}`)
+    .addLabel('impact')
+    .to('#goal-wheel-2', { rotation: 180, duration: duration },`impact${labelOutTiming}`)
+    .add(gsap.to("#engagement",labelOutState),`impact${labelOutTiming}`)
+    .add(gsap.to("#impact",labelInState),`impact${labelInTiming}`)
+    .addLabel('interaction')
+    .to('#goal-wheel-2', { rotation: 270, duration: duration },`interaction${labelOutTiming}`)
+    .add(gsap.to("#impact",labelOutState),`interaction${labelOutTiming}`)
+    .add(gsap.to("#interaction",labelInState),`interaction${labelInTiming}`)
+    .addLabel('end2')
+    .to('#goal-wheel-2', { rotation: 360, duration: duration })
+    .add(gsap.to("#interaction",labelOutState),`end2${labelOutTiming}`)
     
 
   // Priroities slider
